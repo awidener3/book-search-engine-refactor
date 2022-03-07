bd87babadc1971a9cceab976cacd8b24d7ed032a
Alex Widener <awidener3@gmail.com>
2022-03-06 20:59:53 -0500

    added in queries and mutations to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,69 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+	Query: {
+		// Get single user
+		getSingleUser: async (parent, { username }) => {
+			return User.findOne({ username }).populate('books');
+		},
+	},
+	Mutation: {
+		// Create user
+		createUser: async (parent, { username, email, password }) => {
+			const user = await User.create({ username, email, password });
+			const token = signToken(user);
+			return { token, user };
+		},
+		// Login user, sign token, send back
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				throw new AuthenticationError(
+					'No user found with this email address'
+				);
+			}
+
+			const correctPw = await user.isCorrectPassword(password);
+
+			if (!correctPw) {
+				throw new AuthenticationError('Incorrect credentials');
+			}
+
+			const token = signToken(user);
+
+			return { token, user };
+		},
+		// Save book
+		saveBook: async (parent, { user, body }) => {
+			const updatedUser = await User.findOneAndUpdate(
+				{ _id: user._id },
+				{ $addToSet: { savedBooks: body } },
+				{ new: true, runValidators: true }
+			);
+
+			return updatedUser;
+		},
+		// Delete book
+		deleteBook: async (parent, { bookId }, context) => {
+			if (context.user) {
+				const book = await Book.findOneAndDelete({
+					_id: bookId,
+				});
+
+				await User.findOneAndUpdate(
+					{ _id: context.user._id },
+					{ $pull: { savedBooks: { bookId: bookId } } },
+					{ new: true }
+				);
+
+				return book;
+			}
+			throw new AuthenticationError('You need to be logged in!');
+		},
+	},
+};
+
+module.exports = resolvers;
