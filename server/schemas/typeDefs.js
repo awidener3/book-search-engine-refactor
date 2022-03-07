45cae3c29f679c455b9b3cf360c8f91942de6ee1
Alex Widener <awidener3@gmail.com>
2022-03-06 20:41:30 -0500

    Created schemas folder, and added files for resolvers and typeDefs. Completed code for typeDefs.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+	input BookInput {
+		bookId: String
+		authors: [String]
+		description: String
+		title: String
+		image: String
+		link: String
+	}
+
+	type User {
+		_id: ID
+		username: String
+		email: String
+		bookCount: String
+		savedBooks: [Book]!
+	}
+
+	type Book {
+		bookId: String
+		authors: [String]
+		description: String
+		title: String
+		image: String
+		link: String
+	}
+
+	type Auth {
+		token: ID!
+		user: User
+	}
+
+	type Query {
+		me: User
+	}
+
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String!): Auth
+		saveBook(input: BookInput): User
+		removeBook(bookId: String!): User
+	}
+`;
+
+// TODO: Look into line 42, especially with the array of authors in the input type
+
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const express = require('express');
-// Bring in Apollo Server package
+// Bring in apollo server package
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+//  JWT authentication
+const { authMiddleware } = require('./utils/auth');
 
+//  Needed for apollo
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+// ? not needed
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// Apollo server
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,10 +29,24 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-	app.listen(PORT, () =>
-		console.log(`🌍 Now listening on localhost:${PORT}`)
-	);
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+// ? not needed
+// app.use(routes);
+
+// New apollo instance with GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+	await server.start();
+	server.applyMiddleware({ app });
+
+	db.once('open', () => {
+		app.listen(PORT, () =>
+			console.log(`🌍 Now listening on localhost:${PORT}`)
+		);
+	});
+};
+
+// Start server
+startApolloServer(typeDefs, resolvers);
